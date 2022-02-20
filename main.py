040f33c410a2d48793594d0fee047330849227c6
bosnianpopcat <100023437+bosnianpopcat@users.noreply.github.com>
2022-02-20 21:05:31 +0100

    Added program and an example script.

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+print('Enter name of the file with the script you want bspammer to spam (not including extension, extension must be .txt:')
+fname = input()
+print('You have 5 seconds to select the type field you want the bot to spam in before the bot starts spamming.')
+time.sleep(5)
+f = open(fname + ".txt", "r")
+for word in f:
+	pyautogui.typewrite(word)
+	pyautogui.press("enter")
+	time.sleep(0.5)
