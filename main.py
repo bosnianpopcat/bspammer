8f199eadc245c777212725a8a036a8f1d7c12ab0
bosnianpopcat <erikjakinn@gmail.com>
2022-02-21 08:58:42 +0100

    Made a bunch of changes to make the bot work better

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pyautogui
 import time
-print('Enter name of the file with the script you want bspammer to spam (not including extension, extension must be .txt:')
+print('Enter name of the file with the script you want bspammer to spam (not including extension, extension must be .txt):')
 fname = input()
-print('You have 5 seconds to select the type field you want the bot to spam in before the bot starts spamming.')
-time.sleep(5)
+print('Delay between messages (in seconds):')
+dtime = input()
+print('Time to select the chat box you want the bot to spam in before the bot starts spamming (in seconds):')
+wtime = input()
+time.sleep(wtime)
 f = open(fname + ".txt", "r")
 for word in f:
 	pyautogui.typewrite(word)
 	pyautogui.press("enter")
-	time.sleep(0.4)
+	time.sleep(dtime)
